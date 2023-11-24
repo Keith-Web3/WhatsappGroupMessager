22ac63277e3d1846f9e71216d28e224d3bcbaab9
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2023-11-24 16:53:31 +0100

    fix: message new contacts only && perf: optimized for messaging one group at a time

--- index.js
+++ index.js
@@ -26,32 +26,31 @@ client.on('qr', qr => {
 client.on('ready', async () => {
   console.log('Client is ready!')
   const chats = await client.getChats()
-  const groups = chats
-    .filter(
-      chat =>
-        chat.isGroup && chat.name?.toLowerCase().includes('TARGET_GROUP_NAME')
-    )
-    .map(chat => {
-      const MESSAGE = `Enter your message here: ${chat.name}`
-      return [new GroupChat(client, chat), MESSAGE]
-    })
+  const chat = chats.find(
+    chat =>
+      chat.isGroup &&
+      chat.name?.toLowerCase().includes('scientists arena brainstorm')
+  )
 
-  groups.forEach(group => {
-    const participants = group[0].participants
+  const MESSAGE = `Hey 👋, it's Mide!.\nI'm Connecting with many people and would love to connect with you too. Got your contact from ${chat.name}. Let's build a network of friends focused on sharing meaningful values.\nSave my number as "Mide" and send me your name so I can save yours. 😇`
+  const group = [new GroupChat(client, chat), MESSAGE]
 
-    participants.forEach(async (participant, idx) => {
-      console.log('Participant' + idx)
-      const contact = await client.getContactById(participant.id._serialized)
-      const chat = await contact.getChat()
+  console.log(group)
 
-      console.log(chat?.lastMessage)
+  const participants = group[0].participants
 
-      const isNewChat = chat?.lastMessage === undefined
+  participants.forEach(async (participant, idx) => {
+    console.log('Participant' + idx)
+    const contact = await client.getContactById(participant.id._serialized)
+    const chat = await contact.getChat()
 
-      if (isNewChat || !chat) return
-      console.log('Messaging participant' + idx)
-      await chat.sendMessage(group[1])
-    })
+    console.log(chat?.lastMessage === undefined ? 'newContact' : 'old contact')
+
+    const isNewChat = chat?.lastMessage === undefined
+
+    if (!isNewChat || !chat) return
+    console.log('Messaging participant' + idx)
+    await chat.sendMessage(group[1])
   })
 })
 
